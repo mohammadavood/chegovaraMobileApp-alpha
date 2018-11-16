a1ed4a052d654eb5112e2716b0d3377525514327
mohammadavood <davoooooooooooooood@gmail.com>
2018-11-17 00:52:10 +0330

    config andorid code push with npm link

--- android/app/src/main/java/com/chegovaraa/MainApplication.java
+++ android/app/src/main/java/com/chegovaraa/MainApplication.java
@@ -3,6 +3,7 @@ package com.chegovaraa;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -11,32 +12,25 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
-import com.microsoft.codepush.react.CodePush;//add it for CodePush's config
-import com.microsoft.codepush.react.ReactInstanceHolder;//add it for CodePush's config
-
-public class MyReactNativeHost extends ReactNativeHost implements ReactInstanceHolder {//add it for CodePush's config
-  // ... usual overrides
-}
-
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+        @Override
+        protected String getJSBundleFile() {
+        return CodePush.getJSBundleFile();
+        }
+    
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
-    }
-
-    //add this @override for CodePush's config
-    @Override
-    protected String getJSBundleFile() {
-        return CodePush.getJSBundleFile();
     }
 
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new CodePush("oL0HqfhM6GGFUedBEQb4E63DR5mtSJ3DEN4TX", MainApplication.this, BuildConfig.DEBUG)//add this line for CodePush's config
+            new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG)
       );
     }
 
@@ -53,7 +47,6 @@ public class MainApplication extends Application implements ReactApplication {
 
   @Override
   public void onCreate() {
-    CodePush.setReactInstanceHolder(mReactNativeHost);//add it for CodePush's config
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
   }
