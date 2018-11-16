6d8c469cef9b245325fea729ce6aa6dec1caa1a8
mohammadavood <davoooooooooooooood@gmail.com>
2018-11-17 01:29:40 +0330

    implement auto update in app.js for testing

--- App.js
+++ App.js
@@ -8,16 +8,17 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import codePush from 'react-native-code-push';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
-    'Double tap R on your keyboard to reload,\n' +
+    'Double tap Rr on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
 
 type Props = {};
-export default class App extends Component<Props> {
+class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
@@ -28,9 +29,11 @@ export default class App extends Component<Props> {
     );
   }
 }
+const codePushWrapped = codePush(App)
+export default codePushWrapped
 
 const styles = StyleSheet.create({
-  container: {
+  container: {  
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
