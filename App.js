4284c408fd1359533aa7c4dbfaf03a951e85e867
mohammadavood <davoooooooooooooood@gmail.com>
2018-11-20 14:17:24 +0330

    appcenter push notification work well!without react-native-push-notification. it' worked well two last commit,too. but we need react-native-push-notification library for display more custome message on mobile and local sceduled notifications

--- App.js
+++ App.js
@@ -7,8 +7,11 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View,AppState} from 'react-native';
+
 import codePush from 'react-native-code-push';
+import Push from 'appcenter-push'
+
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -24,6 +27,9 @@ class App extends Component<Props> {
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
@@ -31,7 +37,45 @@ class App extends Component<Props> {
 }
 const codePushWrapped = codePush(App)
 export default codePushWrapped
+//---------------------------
 
+Push.setListener({
+  onPushNotificationReceived: function (pushNotification) {
+    let message = pushNotification.message;
+    let title = pushNotification.title;
+    console.log('notif received in main func---------------------------------------------------')
+    console.log("pushNotification  :  ",pushNotification )
+    if (message === null) {
+      // Android messages received in the background don't include a message. On Android, that fact can be used to
+      // check if the message was received in the background or foreground. For iOS the message is always present.
+      title = 'Android background';
+      message = '<empty>';
+      console.log('notif received in null---------------------------------------------------')
+    }
+
+    // Custom name/value pairs set in the App Center web portal are in customProperties
+    if (pushNotification.customProperties && Object.keys(pushNotification.customProperties).length > 0) {
+      message += '\nCustom properties:\n' + JSON.stringify(pushNotification.customProperties);
+      console.log('notif received in custom prop---------------------------------------------------')
+    }
+
+    if (AppState.currentState === 'active') {
+      Alert.alert(title, message);
+      console.log('notif received in forground---------------------------------------------------')
+    }
+    else {
+      console.log('notif received in background--------------------------------------------------')
+      // Sometimes the push callback is received shortly before the app is fully active in the foreground.
+      // In this case you'll want to save off the notification info and wait until the app is fully shown
+      // in the foreground before displaying any UI. You could use AppState.addEventListener to be notified
+      // when the app is fully in the foreground.
+    }
+  }
+});
+
+
+
+// -------------------------
 const styles = StyleSheet.create({
   container: {  
     flex: 1,
